49f45fa9bcab77e46961c5a3d36f05ece1033da2
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-23 16:57:20 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,15 +1,26 @@
 # Code your solution here!
 
 def run_guessing_game()
-  random_number = rand(1..6)
+  #random_number = rand(1..6)
   #puts "random number #{random_number}"
+  user_input = nil
+  random_number = 0
   
-  puts "Guess a number between 1 and 6."
-  user_input = gets.chomp
+  #puts "Guess a number between 1 and 6."
+  #user_input = gets.chomp
   
-  #while user_input.to_f != random_number && user_input != "exit" 
-   # puts "Guess a number between 1 and 6."
-    #user_input = gets.chomp
+  while user_input.to_f != random_number && user_input != "exit" 
+   random_number = rand(1..6)
+   puts "Guess a number between 1 and 6."
+   user_input = gets.chomp
+     if user_input == random_number
+       "You guessed the correct number!"
+     if user_input == "exit"
+       "Goodbye!"
+     else
+       "The computer guessed #{randon_number}"
+  end     
+     
     #if user_input.to_f != random_number
      # puts "The computer guessed #{rand(1..6)}"
     #end  
@@ -17,13 +28,13 @@ def run_guessing_game()
   # end
   
   
-  if user_input.to_f == random_number
-    puts "You guessed the correct number!"
-  elsif user_input == "exit"
-    puts "Goodbye!"  
-  else
-    puts "The computer guessed #{random_number.to_s}."
-  end
+  #if user_input.to_f == random_number
+   # puts "You guessed the correct number!"
+  #elsif user_input == "exit"
+   # puts "Goodbye!"  
+  #else
+   # puts "The computer guessed #{random_number.to_s}."
+  #end
 
 end  
 
