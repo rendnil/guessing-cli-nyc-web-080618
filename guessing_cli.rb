469c6fd0bfeb37f326446d461b7febcd4f1133f7
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-23 17:00:22 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -18,7 +18,7 @@ def run_guessing_game()
      elsif user_input == "exit"
        puts "Goodbye!"
      else
-       puts "The computer guessed #{random_number}"
+       puts "The computer guessed #{random_number}."
   end
 end
      
